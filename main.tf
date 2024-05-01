bea080a31497386416c61d7d0547d7382081a1b6
plzdontbanme <github.anon.demo@gmail.com>
2024-05-01 15:25:47 -0400

    Test signing

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 module "assertion" {
-  source  = "plzdontbanme/assertion/null"
+  source = "plzdontbanme/assertion/null"
   //source = "../terraform-null-assertion"
   version = "0.1.0"
 
-  condition = (1 + 2 == 3)
+  condition     = (1 + 2 == 3)
   error_message = "Your math is invalid"
 }
+
