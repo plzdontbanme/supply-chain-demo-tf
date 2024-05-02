bf3fac3528cae8bf6df95b809a110945f544e2e4
plzdontbanme <github.anon.demo@gmail.com>
2024-05-02 15:48:13 -0400

    Finalize the demo

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  cloud {
-    organization = "ILLiveDemos"
-
-    workspaces {
-      name = "supply-chain-demo-tf-cli"
-    }
-  }
-}
--- main.tf
+++ main.tf
@@ -1,8 +1,31 @@
-module "assertion" {
-  source = "../terraform-null-assertion"
-  //source = "plzdontbanme/assertion/null"
-  //version = "0.1.0"
-
-  condition     = (1 + 2 == 3)
-  error_message = "Your math is invalid"
+# Use Terraform Cloud for state management
+terraform {
+  cloud {
+    organization = "ILLiveDemos"
+    workspaces {
+      name = "supply-chain-demo-tf-cli"
+    }
+  }
 }
+
+# Configure an AWS provider
+provider "aws" {
+  region  = "ca-central-1"
+  profile = "SupplyChainAttackDemo"
+}
+
+# Get the list of availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Check that the desired AZ is available
+module "assertion" {
+  source  = "plzdontbanme/assertion/null"
+  version = "0.1.0"
+
+  condition     = contains(data.aws_availability_zones.available.names, "ca-central-1a")
+  error_message = "The desired availability zone is not available"
+}
+
+# .... do other things, like deploy resources to the selected AZ
