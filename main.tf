dbf4756b06189b1b952f7cc5780d59046189a488
plzdontbanme <github.anon.demo@gmail.com>
2024-05-03 14:11:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ terraform {
 
 # Configure an AWS provider
 provider "aws" {
-  region  = "ca-central-1"
-  profile = "SupplyChainAttackDemo"
+  region = "ca-central-1"
 }
 
-# Get the list of availability zones
+# Get the list of availability zonesassert
 data "aws_availability_zones" "available" {
   state = "available"
 }
@@ -22,7 +21,7 @@ data "aws_availability_zones" "available" {
 # Check that the desired AZ is available
 module "assertion" {
   source  = "plzdontbanme/assertion/null"
-  version = "0.1.0"
+  version = "0.2.0"
 
   condition     = contains(data.aws_availability_zones.available.names, "ca-central-1a")
   error_message = "The desired availability zone is not available"
